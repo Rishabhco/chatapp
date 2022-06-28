3266abe1b22b5a61f74e0e84d3f66db1908c8c08
Rishabh Agrawal <83543522+Rishabhco@users.noreply.github.com>
2022-06-28 16:32:06 +0530

    cleaning the code

--- src/utils/users.js
+++ src/utils/users.js
@@ -3,23 +3,19 @@ const users=[]
 const addUser=({id,username,room})=>{
     username=username.trim().toLowerCase();
     room=room.trim().toLowerCase();
-
     if(!username || !room){
         return {
             error:"Username and room both are required !!"
         }
     }
-
     const existingUser=users.find((user)=>{
         return user.room ===room && user.username ===username
     })
-
     if(existingUser){
         return {
             error:"Username is already taken !!"    
         }
     }
-
     const user={id,username,room};
     users.push(user);
     return {user};
