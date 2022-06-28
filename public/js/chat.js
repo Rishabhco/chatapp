22ae2f964bc4da65ae577dc3e2f27ff64c40d736
Rishabh Agrawal <83543522+Rishabhco@users.noreply.github.com>
2022-06-28 14:46:17 +0530

    started making chat app

---
+++ public/js/chat.js
@@ -0,0 +1,60 @@
+const socket=io()
+
+const $=document.querySelector("#message-form");
+const $formInput=$.querySelector("input");
+const $formButton=$.querySelector("button");
+const sendLocation=document.querySelector("#send-location");
+const $messages=document.querySelector("#messages");
+
+const $msgTemplate=document.querySelector("#message-template").innerHTML;
+const $locationTemplate=document.querySelector("#location-template").innerHTML;
+
+$.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    $formButton.setAttribute("disabled","disabled");
+    const msg=e.target.elements.msg.value;
+    socket.emit("sendMessage",msg,(error)=>{
+        $formButton.removeAttribute('disabled');
+        $formInput.value="";
+        $formInput.focus();
+        if(error){
+            return console.log(error);
+        }
+        console.log("Message delivered");
+    });
+})
+
+socket.on("message",(message)=>{
+    const html=Mustache.render($msgTemplate,{
+        message:message.text,
+        createdAt:moment(message.createdAt).format("h:mm a")
+    })
+    $messages.insertAdjacentHTML("beforeend",html);
+})
+
+document.querySelector("#send-location").addEventListener("click",(e)=>{
+    e.preventDefault();
+    sendLocation.setAttribute("disabled","disabled");
+    if(!navigator.geolocation){
+        sendLocation.removeAttribute("disabled");
+        return alert('Geolocation is not supported by your browser');
+    }
+    navigator.geolocation.getCurrentPosition((position)=>{
+        socket.emit("sendLocation",{
+            latitude:position.coords.latitude,
+            longitude:position.coords.longitude,
+        },()=>{
+            sendLocation.removeAttribute("disabled");
+            console.log("Location shared");
+        });
+    })
+})
+
+socket.on('location',(link)=>{
+    console.log("the location is:",link);
+    const html=Mustache.render($locationTemplate,{
+        link:link.url,
+        createdAt:moment(link.createdAt).format("h:mm a")
+    })
+    $messages.insertAdjacentHTML("beforeend",html);
+})
---
+++ src/index.js
@@ -0,0 +1,43 @@
+const path=require('path');
+const http=require("http");
+const express=require('express');
+const socketio=require("socket.io");
+const Filter=require('bad-words');
+const {generateMessage,generateLocMessage}=require('./utils/messages')
+
+const app=express();
+const server=http.createServer(app);
+const io=socketio(server);
+
+const port =process.env.PORT || 3000;
+const publicDirectoryPath=path.join(__dirname,'../public')
+
+app.use(express.static(publicDirectoryPath));
+
+io.on("connection",(socket)=>{
+    console.log("New Web Socket connected");
+    socket.emit("message",generateMessage('Weclome to the chat app !!'));
+    socket.broadcast.emit("message",generateMessage('A new user joined !!'))
+
+    socket.on("sendMessage",(msg,callback)=>{
+        const filter=new Filter();
+        if(filter.isProfane(msg)){
+            return callback("Profanity is not allowed");
+        }
+        io.emit("message",generateMessage(msg));
+        callback();
+    })
+
+    socket.on("disconnect",()=>{
+        io.emit("message",generateMessage('The user left !!'));
+    })
+
+    socket.on("sendLocation",(coords,callback)=>{
+        io.emit("location",generateLocMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`));
+        callback();
+    })
+})
+
+server.listen(port,()=>{
+    console.log(`Server is up on port ${port}`);
+})
